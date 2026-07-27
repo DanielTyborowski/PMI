b26eb35dcd4165149d484e8e8f0d15ea468e8736
DanielTyborowski <67106801+DanielTyborowski@users.noreply.github.com>
2026-07-27 12:27:42 +0200

    Grundstruktur

---
+++ pmi/database/seeders/postItsSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class postItsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+    }
+}
